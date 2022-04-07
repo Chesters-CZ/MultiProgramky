8dac52d6bf03c273486e7f59e45d0ccf12905e84
Chesters-CZ <62833819+Chesters-CZ@users.noreply.github.com>
2022-04-07 09:09:08 +0200

    mfw debil

--- src/cz/chesters/multiprogramky/prumery/Prumery.java
+++ src/cz/chesters/multiprogramky/prumery/Prumery.java
@@ -11,25 +11,25 @@ public class Prumery {
         Scanner scanner = new Scanner(System.in);
         String inp;
 
-        System.out.println(Mik.BG_BLUE + Mik.BLACK + "Vítej v Průměrech" + Mik.RESET);
-        System.out.println(Mik.BLUE + "Zadej první číslo" + Mik.RESET);
+        System.out.println(Mik.BG_PURPLE + Mik.BLACK + "Vítej v Kameni, nůžkách a papíru." + Mik.RESET);
+        System.out.println(Mik.PURPLE + "Zadej první číslo" + Mik.RESET);
         cisla.add(scanner.nextInt());
         System.out.println();
 
         while (true) {
-            System.out.println(Mik.BLUE + "Chceš zadat další číslo? y/n" + Mik.RESET);
+            System.out.println(Mik.PURPLE + "Chceš zadat další číslo? y/n" + Mik.RESET);
             //noinspection UnusedAssignment
             inp = scanner.nextLine(); // musí tady bejt, jinak to hází errory
             inp = scanner.nextLine();
             if (inp.toLowerCase().charAt(0) != 'y')
                 break;
 
-            System.out.println(Mik.BLUE + "Zadej další číslo" + Mik.RESET);
+            System.out.println(Mik.PURPLE + "Zadej další číslo" + Mik.RESET);
             cisla.add(scanner.nextInt());
             System.out.println();
         }
 
-        System.out.println(Mik.BLUE + "Průměr těchto " + cisla.size() + " čísel je " + (sectiArrayList(cisla) / cisla.size()) + Mik.RESET);
+        System.out.println(Mik.PURPLE + "Průměr těchto " + cisla.size() + " čísel je " + (sectiArrayList(cisla) / cisla.size()) + Mik.RESET);
         System.out.println();
     }
 
